8f6b17139df878310dfe4bae6a061ee367b2b199
vivekpal1 <31vivekpal@gmail.com>
2024-07-22 14:24:45 +0530

    fix: image url

--- src/app/api/actions/wink/route.ts
+++ src/app/api/actions/wink/route.ts
@@ -23,7 +23,7 @@ export const GET = async (req: Request) => {
 
     const payload: ActionGetResponse = {
       title: "Interact with a Twitter User",
-      icon: '/bg.png',
+      icon: 'https://winked.vercel.app/bg.png',
       description: description,
       label: "Send Message and Bid",
       links: {
